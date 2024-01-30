b10003d4d710842aaabb47db07fbc6785b96c83f
SebastianG03 <guamandavid11235@gmail.com>
2024-01-29 22:17:16 -0500

    Iterator creado

---
+++ BlazorApp1/Models/Iterator/EmisorCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace BlazorApp1.Models.Iterator
+{
+    public class EmisorCollection : IEnumerable<Emisor>
+    {
+        private List<Emisor> emisores = new List<Emisor>();
+
+        public void AddEmisor(Emisor emisor)
+        {
+            emisores.Add(emisor);
+        }
+
+
+        public IEnumerator<Emisor> GetEnumerator()
+        {
+            return emisores.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new EmisorIterator(emisores);
+        }
+    }
+}
